ba4f55a8b2be0337262eb08d891f2cdc4dfcacea
temitayoadefemi <111149469+temitayoadefemi@users.noreply.github.com>
2024-07-07 09:44:04 +0100

    Update server.c

--- server.c
+++ server.c
@@ -9,11 +9,9 @@
 #define PORT 8080
 #define BACKLOG 10
 
-
 void handle_client(int client_socket) {
     char buffer[1024];
     int bytes_read = read(client_socket, buffer, sizeof(buffer) - 1);
-
 
     if (bytes_read < 0) {
         perror("read failed");
@@ -22,35 +20,41 @@ void handle_client(int client_socket) {
     }
 
     buffer[bytes_read] = '\0';
-    printf("Request: %s\n", buffer);
+    printf("%s", buffer);
 
-    const char *response = 
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/html\r\n"
-        "Content-Length: 51\r\n"
-        "\r\n"
-        "<html><body><h1>Hello, World!</h1></body><html>";
+    char method[16], path[256], version[16];
+    sscanf(buffer, "%s %s %s", method, path, version);
 
-    write(client_socket, response, strlen(response));
-    close(client_socket);
+    const char *html_content = "<html><body><h1>Hello, World!</h1></body></html>";
+    int content_length = strlen(html_content);  // Calculate the length of the HTML content
+
+    char response[1024];
+    int response_length = snprintf(response, sizeof(response),
+
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/html\r\n"
+    "Content-Length: %d\r\n"  // Use the calculated length
+    "\r\n"
+    "%s", content_length, html_content);
+
+
+
+    write(client_socket, response, response_length);
 
 }
 
-
 int main(int argc, char *argv[]) {
-
     int server_fd, new_socket;
     struct sockaddr_in server_addr, client_addr;
     socklen_t addr_len = sizeof(client_addr);
     int opt = 1;
-
 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt))) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("setsockopt failed");
         close(server_fd);
         exit(EXIT_FAILURE);
@@ -59,7 +63,6 @@ int main(int argc, char *argv[]) {
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
-
 
     if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("bind failed");
@@ -71,13 +74,11 @@ int main(int argc, char *argv[]) {
         perror("listen failed");
         close(server_fd);
         exit(EXIT_FAILURE);
-
     }
 
     printf("HTTP Server listening on port %d\n", PORT);
 
-    while(1) {
-
+    while (1) {
         if ((new_socket = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len)) < 0) {
             perror("accept failed");
             close(server_fd);
@@ -88,6 +89,5 @@ int main(int argc, char *argv[]) {
     }
 
     close(server_fd);
-
     return 0;
 }
