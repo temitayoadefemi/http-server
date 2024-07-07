7e1c029f81dae8caedfb9dc01a192c31c2c6a827
temitayoadefemi <111149469+temitayoadefemi@users.noreply.github.com>
2024-07-07 09:04:28 +0100

    Update server.c

--- server.c
+++ server.c
@@ -9,66 +9,84 @@
 #define PORT 8080
 #define BACKLOG 10
 
+
+void handle_client(int client_socket) {
+    char buffer[1024];
+    int bytes_read = read(client_socket, buffer, sizeof(buffer) - 1);
+
+
+    if (bytes_read < 0) {
+        perror("read failed");
+        close(client_socket);
+        return;
+    }
+
+    buffer[bytes_read] = '\0';
+    printf("Request: %s\n", buffer);
+
+    const char *response = 
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html\r\n"
+        "Content-Length: 51\r\n"
+        "\r\n"
+        "<html><body><h1>Hello, World!</h1></body><html>";
+
+    write(client_socket, response, strlen(response));
+    close(client_socket);
+
+}
+
+
 int main(int argc, char *argv[]) {
+
     int server_fd, new_socket;
     struct sockaddr_in server_addr, client_addr;
     socklen_t addr_len = sizeof(client_addr);
-    char buffer[1024] = {0};
     int opt = 1;
 
-    // Create socket file descriptor
+
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Set socket options
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt))) {
         perror("setsockopt failed");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
 
-    // Initialize server address structure
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
-    // Bind the socket to the network address and port
+
     if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("bind failed");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
 
-    // Listen for incoming connections
     if (listen(server_fd, BACKLOG) < 0) {
         perror("listen failed");
         close(server_fd);
         exit(EXIT_FAILURE);
+
     }
 
-    printf("Server listening on port %d\n", PORT);
+    printf("HTTP Server listening on port %d\n", PORT);
 
-    // Accept an incoming connection
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len)) < 0) {
-        perror("accept failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
+    while(1) {
+
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len)) < 0) {
+            perror("accept failed");
+            close(server_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        handle_client(new_socket);
     }
 
-    printf("Connection accepted\n");
-
-    // Read and print message from client
-    read(new_socket, buffer, sizeof(buffer));
-    printf("Message from client: %s\n", buffer);
-
-    // Send response to client
-    char *response = "Hello from server";
-    send(new_socket, response, strlen(response), 0);
-
-    // Close sockets
-    close(new_socket);
     close(server_fd);
 
     return 0;
