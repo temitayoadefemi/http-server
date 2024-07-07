bc01977475bbb1724a2a0f2f1df326fdaacbc55b
temitayoadefemi <111149469+temitayoadefemi@users.noreply.github.com>
2024-07-07 07:16:23 +0100

    Create server.c

---
+++ server.c
@@ -0,0 +1,32 @@
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <regex.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+
+    int server_fd;
+    struct sockaddr server_addr;
+
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+
+
+
+}
